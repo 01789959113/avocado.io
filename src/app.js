c8138339a0fe3ed8cae94e2f937269cafb423134
anmsakib <anmsakib999@gmail.com>
2021-09-02 21:03:11 +0600

    edited in three

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-require('./db/connect')
-const User = require('./models/model')
+require('../db/connect')
+const User = require('../models/model')
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
