47c9308702421d5a94f43f32a5dac97b7119da2e
anmsakib <anmsakib999@gmail.com>
2021-09-03 10:07:37 +0600

    reomve hostname

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 require('./db/connect')
 const User = require('./models/model')
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 
@@ -287,6 +287,10 @@ app.get('*', (req, res)=>{
 
 
 // server listening to port
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// app.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.listen(port, () => {
+    console.log(`Server running at ${port}`);
 });
